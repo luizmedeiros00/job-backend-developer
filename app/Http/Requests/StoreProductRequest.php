f4d345b264e81ef0fdac5d440981a8274a8194de
luiz medeiros <luizmedeiros00@gmail.com>
2022-11-12 16:07:51 -0300

    validate request products

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+           'name'           => 'required|unique:products',
+           'price'          => 'required|numeric',
+           'description'    => 'required',
+           'category'       => 'required',
+           'image_url'      => 'nullable|url'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required'         => 'O nome é obrigatório',
+            'name.unique'           => 'O nome ja existe',
+            'price.required'        => 'O preço é obrigatório',
+            'price.numeric'         => 'O preço deve ser do tipo numérico',
+            'description.required'  => 'A descrição é obrigatória',
+            'category.required'     => 'A categoria é obrigatória',
+            'image_url.url'         => 'Image url inválida'
+         ];
+    }
+}
